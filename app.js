c63cb4ce948b4306d590ec73e1bdf2be28e9f279
Hang0213 <hanghao0213@gmail.com>
2022-09-27 17:49:43 -0400

    final

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const apiRequest = async () => {
    */
 
   // TODO fill in your own port number 
-  const PORT_NUMBER = "";
+  const PORT_NUMBER = "8010";
 
   const baseUrl = `http://localhost:${PORT_NUMBER}/proxy/api/`
 
@@ -40,13 +40,38 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15", 
+  const filtedFruitsArray = fruitsArray.filter((item) => 
+    item.nutritions.sugar > 15
+  );
+  console.log(filtedFruitsArray)
+  const fruitNameArrary = filtedFruitsArray.map(item => item.name);
 
   // TODO: Create a new HTML element to display your data 
+  const fruitElement = document.getElementById('fruit');
 
+  const fruitDisplayElement = document.createElement('div');
+  fruitElement.append(fruitDisplayElement);
+
+
+  // function createFruitItem(fruitName){
+  //   let li = document.createElement('li');
+  //   li.innerText = fruitName;
+  //   return li
+  // }
+  const fruitList = document.createElement('ul');
+  fruitDisplayElement.appendChild(fruitList)
+  console.log(fruitNameArrary)
+  for(let i = 0; i < fruitNameArrary.length; i++){
+    let li = document.createElement('li');
+    li.innerHTML = fruitNameArrary[i];
+    console.log(fruitNameArrary[i]);
+    fruitList.appendChild(li);
+  }
+  
   // TODO: Append your new element to the page
 
 }
